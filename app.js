1161be20b032e15fc15c2957e89a668da4071c21
Kreutzer Hellscythe <elijahlancecatli@gmail.com>
2020-06-03 22:43:54 +0800

    Added Database

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 let express = require("express");
 app = express();
+port = process.env.PORT || 3000;
+
+//Mongo Atlas Driver
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+	"mongodb+srv://Lance:admin@todo-list-app-cluster-0xay8.azure.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+	const collection = client.db("test").collection("devices");
+	// perform actions on the collection object
+	client.close();
+});
 
 app.get("/", (req, res) => {
 	res.send("TESTING");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
 	console.log(`Server started on port ` + process.env.PORT);
 });
