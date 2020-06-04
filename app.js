23029bd4e20377d21da55b3c6b9cd09dc7d8ee0d
Kreutzer Hellscythe <elijahlancecatli@gmail.com>
2020-06-04 17:13:19 +0800

    Added HTML and CSS

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ let port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));
 
 //Routes
 let todoRoutes = require("./routes/todos");
 
 app.get("/", (req, res) => {
-	res.send("Hello from the root route");
+	res.render("index.html");
 });
 
 //Using routes
